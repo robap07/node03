01a967fb91b6bc560abcf7538c0365cf4c89e36f
robap07 <joserobertomonteirofaria@gmail.com>
2024-12-26 22:13:40 -0300

    Add files via upload
    xaxaxax

--- server.js
+++ server.js
@@ -26,9 +26,15 @@ async function printScreen() {
 
     const page = await browser.newPage();
 
-    // Acessa a URL desejada
-    await page.goto('https://www.windy.com/pt/-Chuva-trov%C3%A3o-rain?rain,2024122921,-22.451,-42.681,8');
-    console.log('Abriu o navegador e acessou a Netflix');
+// Acessa a URL desejada
+await page.goto('https://www.windy.com/pt/-Chuva-trov%C3%A3o-rain?rain,2024122921,-22.451,-42.681,8', {
+  waitUntil: 'networkidle2', // Aguarda até que a rede esteja ociosa
+  timeout: 60000, // Tempo máximo para a página carregar (60 segundos)
+});
+console.log('Aguardando o carregamento da página...');
+await page.waitForTimeout(10000); // Aguarda 10 segundos adicionais
+console.log('Abriu o navegador e acessou o site.');
+
 
     // Define o caminho para salvar a imagem
     const timestamp = Date.now();
